b44c38b707b44dad807975cffb771b72dc759410
Leon <145806929+liyue4096@users.noreply.github.com>
2024-03-27 21:56:24 -0400

    Update final_project.sql

--- final_project.sql
+++ final_project.sql
@@ -3,4 +3,18 @@ CREATE TABLE usertype (
     user_type VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE Users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password_hash VARCHAR(255) NOT NULL,
+    date_joined DATETIME NOT NULL,
+    last_login DATETIME,
+    fk_user_type_id INT,
+    FOREIGN KEY (fk_user_type_id) REFERENCES UserType(user_type_id)
+);
+
+CREATE INDEX idx_email ON Users(email);
+CREATE INDEX idx_username ON Users(user_name);
+
 CREATE TABLE 
